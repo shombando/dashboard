8a4335d22b40f4ffe969b29ca70fb092627cdfd3
Shom Bandopadhaya <shom@bandopadhaya.com>
2023-07-17 23:34:16 -0500

    Added EJS to render HTML, added pico CSS but not working
    having issues with serving CSS locally

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express, {Express, Request, Response} from "express";
 const port = 9876;
-
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World from TS live");
-});
+app.set('view engine', 'ejs');
+//app.use(express.static(__dirname + '/public'));
+//app.set('views', __dirname + '/views');
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
-})
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.render("index");
+});
+
