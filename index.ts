d344d48c68464dc615af0f53ff21d4e576279475
Shom Bandopadhaya <shom@bandopadhaya.com>
2023-07-17 23:41:58 -0500

    Going with JS CDN for CSS for now, for the sake of progress

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@ const port = 9876;
 const app: Express = express();
 
 app.set('view engine', 'ejs');
-//app.use(express.static(__dirname + '/public'));
-//app.set('views', __dirname + '/views');
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
